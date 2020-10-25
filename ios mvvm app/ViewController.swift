4d4d84b895d49d141a7c1d8ce4bbe12a0010019f
AhmedElbasha97 <bidoelbasha@gmail.com>
2020-10-25 07:50:45 -0700

    Initial Commit

---
+++ ios mvvm app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios mvvm app
+//
+//  Created by ahmedelbasha on 10/25/20.
+//  Copyright © 2020 ahmedelbasha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
